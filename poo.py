5accaeb839a983e44028d7d642049f960cdc6e89
lucioysantos <161756006+santosmohnylucioaraoz@users.noreply.github.com>
2024-03-04 11:51:00 -0300

    Update poo.py

--- poo.py
+++ poo.py
@@ -1,1 +1,33 @@
-## Crear una clase llamada Vehiculo capza
+## Crear una clase llamada Vehiculo capaz
+
+class Vehiculo:
+  def __init__(self, patente, marca, modelo, kilometraje):
+    self.patente = patente
+    self.marca = marca
+    self.modelo = modelo
+    self.kilometraje = kilometraje
+
+def get_patente(self):
+  return self.patente
+
+def get_marca(self):
+  return self.marca
+
+def get_modelo(self):
+  return self.marca
+
+def get_kilometraje(self):
+  return self.kilometraje
+
+def set_patente(self, nueva patente):
+  self.patente = nueva_patente
+
+def set_marca(self, nueva_marca):
+  self.modelo = nueva_marca 
+
+def set_kilometraje(self, nuevo_kilometraje):
+  self.kilomnetreje = nuevo_kilometraje
+
+mi_vehiculo = Vehiculo("ABC123", "Totyoa
+  
+                    
