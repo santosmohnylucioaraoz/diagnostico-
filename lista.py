061b203cf0a95dd474b92d2b3462bd3a90184a17
lucioysantos <161756006+santosmohnylucioaraoz@users.noreply.github.com>
2024-03-08 11:25:22 -0300

    Create lista.py

---
+++ lista.py
@@ -0,0 +1,7 @@
+## crear una lista con 5 elementos (nombre de países)
+
+paises = ["Argentina", "Francia", "Colombia", "Japon", "Rusia"]
+
+print("Paises:")
+for pais in paises:
+    print("-", pais)
